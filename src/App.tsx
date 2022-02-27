b624e400a920fd4e6703241d353c6d7893b4fc90
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2022-02-27 11:55:14 +0100

    created minimal, basic layout setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
+import { styled } from "@stitches/react"
 import React from "react"
 import { globalStyles } from "./themes"
 
+const MainGrid = styled('div', {
+  display: 'grid',
+  gridTemplateRows: '100px 1fr'
+})
+
+const TopRow = styled('div', {
+  display: 'grid',
+  gridTemplateColumns: '1fr 100px'
+})
+
+const MainRow = styled('div', {})
+
 function App() {
   globalStyles()
-  return <div>Hello</div>
+  return <MainGrid>
+    <TopRow>
+      <div>EntryCreator</div>
+      <div>Settings</div>
+    </TopRow>
+    <MainRow>MainRow</MainRow>
+  </MainGrid>
 }
 
 export default App
