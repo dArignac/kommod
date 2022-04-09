7961c6c964fd327733b96661152967511d40f2b7
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2022-04-09 14:47:59 +0200

    be able to overwrite today's date for testing and development

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,11 @@
 interface Config {
+  overwriteToday: boolean // for testing one can overwrite the date of today
+  overwriteTodayValue: string | null
   storageClass: "LocalStorage" | "StrongholdStorage"
 }
 
 export const config: Config = {
+  overwriteToday: false,
+  overwriteTodayValue: "2022-04-08T00:00:00",
   storageClass: "LocalStorage",
 }
--- src/services/date/DateService.ts
+++ src/services/date/DateService.ts
@@ -1,3 +1,5 @@
+import { config } from "../../config"
+
 export class DateService {
   private static instance: DateService
   private constructor() {}
@@ -12,7 +14,7 @@ export class DateService {
     if (hour < 0 || hour > 23) hour = 0
     if (minute < 0 || minute > 59) minute = 0
     if (second < 0 || second > 59) second = 0
-    let x = new Date()
+    let x = config.overwriteToday ? new Date(config.overwriteTodayValue!!) : new Date()
     return new Date(Date.UTC(x.getUTCFullYear(), x.getUTCMonth(), x.getUTCDate(), hour, minute, second))
   }
 
