f1b0391195f2ed77ca0d63f147c86a92b9da0d88
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2022-04-27 19:41:24 +0200

    test: added mocks for tauri objects
    #37

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,16 +4,16 @@ import React from "react"
 import { App } from "./App"
 
 test("Renders default components", () => {
-  // FIXME re-enable
-  //   render(<App />)
-  //   expect(screen.getByText(/Settings/i)).toBeInTheDocument()
-  //   expect(screen.getByText(/El Toggl/)).toBeInTheDocument()
+  render(<App />)
+  // FIXME behaviour is now async
+  // expect(screen.getByText(/Settings/i)).toBeInTheDocument()
+  // expect(screen.getByText(/El Toggl/)).toBeInTheDocument()
 })
 
 test("Navigation to settings works", () => {
-  // FIXME re-enable
-  //   const leftClick = { button: 0 }
-  //   render(<App />)
+  const leftClick = { button: 0 }
+  render(<App />)
+  // FIXME behaviour is now async
   //   userEvent.click(screen.getByTestId("link-settings"), leftClick)
   //   expect(screen.getByText(/El Toggl - Settings/)).toBeInTheDocument()
   //   userEvent.click(screen.getByTestId("link-home"), leftClick)
