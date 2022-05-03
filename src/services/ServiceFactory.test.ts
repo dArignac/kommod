08726fde7f7047b7cf7c89d0c741ce0eafb5326f
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2022-05-03 18:16:06 +0200

    test: re-enabled tests for ServiceFactory
    #24

--- src/services/ServiceFactory.test.ts
+++ src/services/ServiceFactory.test.ts
@@ -1,21 +1,20 @@
-import { ServiceFactory } from "./ServiceFactory"
 import { config } from "../config"
+import { ServiceFactory } from "./ServiceFactory"
 import { LocalStorage } from "./storage/LocalStorage"
+import { StrongholdStorage } from "./storage/StrongholdStorage"
 
 test("ServiceFactory is a singleton", () => {
-  // FIXME re-enable
-  // const a = ServiceFactory.getInstance()
-  // const b = ServiceFactory.getInstance()
-  // expect(a).toBe(b)
+  const a = ServiceFactory.getInstance()
+  const b = ServiceFactory.getInstance()
+  expect(a).toBe(b)
 })
 
-// test("Return LocalStorage if configured", () => {
-//   config.storageClass = "LocalStorage"
-//   expect(ServiceFactory.getInstance().getStorage()).toBeInstanceOf(LocalStorage)
-// })
+test("Return LocalStorage if configured", () => {
+  config.storageClass = "LocalStorage"
+  expect(ServiceFactory.getInstance().getStorage()).toBeInstanceOf(LocalStorage)
+})
 
-// test("Throw exception when StrongholdStorage is configured", () => {
-//   config.storageClass = "StrongholdStorage"
-//   expect(() => ServiceFactory.getInstance().getStorage()).toThrow(Error)
-//   expect(() => ServiceFactory.getInstance().getStorage()).toThrow("Storage is not implemented for StrongholdStorage")
-// })
+test("Return StrongholdStorage if configured", () => {
+  config.storageClass = "StrongholdStorage"
+  expect(ServiceFactory.getInstance().getStorage()).toBeInstanceOf(StrongholdStorage)
+})
