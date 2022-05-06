b110b5b6de4def129e8512121b9a5a58091a5c5d
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2022-05-06 11:01:12 +0200

    refactor: moved type helper to utils
    #24

--- src/services/date.ts
+++ src/services/date.ts
@@ -4,6 +4,7 @@ import setHours from "date-fns/setHours"
 import setMinutes from "date-fns/setMinutes"
 import setSeconds from "date-fns/setSeconds"
 import { StartStopable } from "../types"
+import { hasOwnProperty } from "../utils"
 
 export function formatDuration(seconds: number): string {
   return format(new Date(1970, 1, 1, 0, 0, seconds), "HH:mm:ss")
@@ -19,10 +20,6 @@ export function setToMidnight(day: Date): Date {
 
 export function setToBeforeMidnight(day: Date): Date {
   return setSeconds(setMinutes(setHours(day, 23), 59), 59)
-}
-
-function hasOwnProperty<X extends {}, Y extends PropertyKey>(obj: X, prop: Y): obj is X & Record<Y, unknown> {
-  return obj.hasOwnProperty(prop)
 }
 
 export function sort<T extends StartStopable>(a: T, b: T): number {
