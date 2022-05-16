6836b6da45c1633fc4d148e71d8f36a9f93e1659
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2022-05-16 20:25:58 +0200

    fix: fixed notification for stronghold storage
    #58

--- src/services/storage/StrongholdStorage.test.ts
+++ src/services/storage/StrongholdStorage.test.ts
@@ -19,6 +19,8 @@ jest.mock("@tauri-apps/api/fs", () => {
   }
 })
 
+const delay = 1000
+
 test("Initializes correctly", async () => {
   const spyStoreGet = jest.spyOn(Store.prototype, "get").mockResolvedValue("initialToken1")
   const spyStoreInsert = jest.spyOn(Store.prototype, "insert")
@@ -52,14 +54,14 @@ test("Changed token gets stored to storage", async () => {
   })
 
   // we need to wait until all async calls are done, but there is nothing to listen to that we can access
-  await new Promise((r) => setTimeout(r, 2000))
+  await new Promise((r) => setTimeout(r, delay))
 
   expect(spyStoreGet).toBeCalledTimes(1)
   expect(spyStoreInsert).toHaveBeenLastCalledWith(expect.anything(), "newToken")
   expect(SettingsStore.getRawState()).toEqual({
     isStorageReady: true,
     token: "newToken",
-    tokenSaveStatus: "na",
+    tokenSaveStatus: "success",
   })
   expect(spyStrongholdSave).toBeCalled()
 
@@ -79,16 +81,45 @@ test("Changed token with same value as before does not get stored to storage", a
   })
 
   // we need to wait until all async calls are done, but there is nothing to listen to that we can access
-  await new Promise((r) => setTimeout(r, 2000))
+  await new Promise((r) => setTimeout(r, delay))
 
   expect(spyStoreGet).toBeCalledTimes(1)
   expect(spyStoreInsert).toBeCalledTimes(0)
   expect(SettingsStore.getRawState()).toEqual({
     isStorageReady: true,
     token: "initialToken3",
-    tokenSaveStatus: "na",
+    tokenSaveStatus: "success",
   })
   expect(spyStrongholdSave).toBeCalledTimes(0)
+
+  storage.cancelStoreSubscription()
+})
+
+test("Failed saving triggers store change", async () => {
+  const spyStoreGet = jest.spyOn(Store.prototype, "get").mockResolvedValue("initialToken3")
+  const spyStoreInsert = jest.spyOn(Store.prototype, "insert").mockReturnValue(Promise.resolve())
+  const spyStrongholdSave = jest.spyOn(Stronghold.prototype, "save").mockImplementation(() => {
+    throw new Error("")
+  })
+
+  const storage = new StrongholdStorage()
+  await storage.initialize()
+
+  SettingsStore.update((s) => {
+    s.token = "initialToken4"
+  })
+
+  // we need to wait until all async calls are done, but there is nothing to listen to that we can access
+  await new Promise((r) => setTimeout(r, delay))
+
+  expect(spyStoreGet).toBeCalledTimes(1)
+  expect(spyStoreInsert).toBeCalledTimes(1)
+  expect(SettingsStore.getRawState()).toEqual({
+    isStorageReady: true,
+    token: "initialToken4",
+    tokenSaveStatus: "error",
+  })
+  expect(spyStrongholdSave).toBeCalledTimes(1)
 
   storage.cancelStoreSubscription()
 })
