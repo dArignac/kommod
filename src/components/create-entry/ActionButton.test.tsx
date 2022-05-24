c44b1154ec1790e2eb370a0e87ff67883fbdea46
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2022-05-24 15:36:12 +0200

    feat: set action button label
    #38

--- src/components/create-entry/ActionButton.test.tsx
+++ src/components/create-entry/ActionButton.test.tsx
@@ -1,6 +1,56 @@
-import { render } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
+import { BookingStore } from "../../store"
 import { ActionButton } from "./ActionButton"
 
+const defaultProps = { tabIndex: 1, width: 100 }
+const getButton = () => screen.getByTestId("action-button") as HTMLButtonElement
+
 test("renders the component", () => {
-  render(<ActionButton tabIndex={1} width={100} />)
+  render(<ActionButton {...defaultProps} />)
+})
+
+test("displays start button with default store", () => {
+  render(<ActionButton {...defaultProps} />)
+  expect(getButton().textContent).toBe("Start")
+})
+
+test("displays start button with setup store (scenario 1)", () => {
+  BookingStore.update((s) => {
+    s.projectId = 1
+    s.timeEntryDescription = "Test"
+  })
+  render(<ActionButton {...defaultProps} />)
+  expect(getButton().textContent).toBe("Start")
+})
+
+test("displays start button with setup store (scenario 2)", () => {
+  BookingStore.update((s) => {
+    s.projectId = 1
+    s.timeStart = "11:30"
+    s.timeEntryDescription = "Test"
+  })
+  render(<ActionButton {...defaultProps} />)
+  expect(getButton().textContent).toBe("Start")
+})
+
+test("displays stop button with setup store (scenario 3)", () => {
+  BookingStore.update((s) => {
+    s.projectId = 1
+    s.timeStart = "11:30"
+    s.timeStop = "12:30"
+    s.timeEntryDescription = "Test"
+  })
+  render(<ActionButton {...defaultProps} />)
+  expect(getButton().textContent).toBe("Stop")
+})
+
+test("displays stop button with setup store (scenario 4)", () => {
+  BookingStore.update((s) => {
+    s.projectId = 1
+    s.timeStart = "11:30"
+    s.timeEntryDescription = "Test"
+    s.timEntryId = 1
+  })
+  render(<ActionButton {...defaultProps} />)
+  expect(getButton().textContent).toBe("Stop")
 })
--- src/components/create-entry/ActionButton.tsx
+++ src/components/create-entry/ActionButton.tsx
@@ -1,4 +1,6 @@
 import { Button } from "antd"
+import { useStoreState } from "pullstate"
+import { BookingStore } from "../../store"
 
 interface ActionButtonProps {
   tabIndex: number
@@ -7,9 +9,18 @@ interface ActionButtonProps {
 
 // FIXME #38 button needs to check all required values and set them to error
 export function ActionButton({ tabIndex, width }: ActionButtonProps) {
+  const timeEntryId = useStoreState(BookingStore, (s) => s.timEntryId)
+  const timeStop = useStoreState(BookingStore, (s) => s.timeStop)
+
+  let label = "Start"
+
+  if (timeStop !== undefined || timeEntryId !== undefined) {
+    label = "Stop"
+  }
+
   return (
-    <Button tabIndex={tabIndex} style={{ width }}>
-      Start
+    <Button data-testid="action-button" tabIndex={tabIndex} style={{ width }}>
+      {label}
     </Button>
   )
 }
