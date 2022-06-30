ff5dd85f9d54d99236324f615b2ba4d0ef2c01cc
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2022-06-30 07:33:13 +0200

    chore: updated packages

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -9,7 +9,11 @@ fn main() {
   let context = tauri::generate_context!();
   tauri::Builder::default()
     .plugin(TauriStronghold::default())
-    .menu(tauri::Menu::os_default(&context.package_info().name))
+    .menu(if cfg!(target_os = "macos") {
+      tauri::Menu::os_default(&context.package_info().name)
+    } else {
+      tauri::Menu::default()
+    })
     .run(context)
     .expect("error while running tauri application");
 }
